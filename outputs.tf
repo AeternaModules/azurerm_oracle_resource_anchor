8f3d2c818aff0db71bdc6e89335a1ae490c586cf
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "oracle_resource_anchors" {
   description = "All oracle_resource_anchor resources"
   value       = azurerm_oracle_resource_anchor.oracle_resource_anchors
 }
+output "oracle_resource_anchors_linked_compartment_id" {
+  description = "List of linked_compartment_id values across all oracle_resource_anchors"
+  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.linked_compartment_id]
+}
+output "oracle_resource_anchors_location" {
+  description = "List of location values across all oracle_resource_anchors"
+  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.location]
+}
+output "oracle_resource_anchors_name" {
+  description = "List of name values across all oracle_resource_anchors"
+  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.name]
+}
+output "oracle_resource_anchors_resource_group_name" {
+  description = "List of resource_group_name values across all oracle_resource_anchors"
+  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.resource_group_name]
+}
+output "oracle_resource_anchors_tags" {
+  description = "List of tags values across all oracle_resource_anchors"
+  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.tags]
+}
 
