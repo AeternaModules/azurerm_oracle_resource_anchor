2e8027e4453f1d9cebe9da9cc213a5590973587d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "oracle_resource_anchors" {
-  description = "All oracle_resource_anchor resources"
-  value       = azurerm_oracle_resource_anchor.oracle_resource_anchors
-}
 output "oracle_resource_anchors_linked_compartment_id" {
-  description = "List of linked_compartment_id values across all oracle_resource_anchors"
-  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.linked_compartment_id]
+  description = "Map of linked_compartment_id values across all oracle_resource_anchors, keyed the same as var.oracle_resource_anchors"
+  value       = { for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : k => v.linked_compartment_id }
 }
 output "oracle_resource_anchors_location" {
-  description = "List of location values across all oracle_resource_anchors"
-  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.location]
+  description = "Map of location values across all oracle_resource_anchors, keyed the same as var.oracle_resource_anchors"
+  value       = { for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : k => v.location }
 }
 output "oracle_resource_anchors_name" {
-  description = "List of name values across all oracle_resource_anchors"
-  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.name]
+  description = "Map of name values across all oracle_resource_anchors, keyed the same as var.oracle_resource_anchors"
+  value       = { for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : k => v.name }
 }
 output "oracle_resource_anchors_resource_group_name" {
-  description = "List of resource_group_name values across all oracle_resource_anchors"
-  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.resource_group_name]
+  description = "Map of resource_group_name values across all oracle_resource_anchors, keyed the same as var.oracle_resource_anchors"
+  value       = { for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : k => v.resource_group_name }
 }
 output "oracle_resource_anchors_tags" {
-  description = "List of tags values across all oracle_resource_anchors"
-  value       = [for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : v.tags]
+  description = "Map of tags values across all oracle_resource_anchors, keyed the same as var.oracle_resource_anchors"
+  value       = { for k, v in azurerm_oracle_resource_anchor.oracle_resource_anchors : k => v.tags }
 }
 
